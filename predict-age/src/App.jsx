62ff4b5853f0787b5df947161c5649a931ceaa4d
IgMiras <im.miras@unesp.br>
2024-01-05 20:43:45 -0300

    predict-age exercise is done

--- predict-age/src/App.jsx
+++ predict-age/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [age, setAge] = useState(0)
+  const [name, setName] = useState('')
+
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+      setAge(res.data.age)
+    })
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <input type="text" placeholder='Ex. Igor...' onChange={handleChange} />
+      <button onClick={fetchData}> Predict Age </button>
+
+      <h1> Predicted Age: {age} </h1>
+    </div>
   )
 }
 
