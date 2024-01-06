7f361da67378c930f4d55ea65b4e552a355f7a78
IgMiras <im.miras@unesp.br>
2024-01-05 21:06:45 -0300

    generate-an-excuse is done

--- generate-an-excuse/src/App.jsx
+++ generate-an-excuse/src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Axios from 'axios'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [excuse, setExcuse] = useState('')
 
+  const fetchData = (category) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${category}`)
+    .then((res) => {
+      console.log(res.data[0].excuse)
+      setExcuse(res.data[0].excuse)
+    })
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <h1> Generate An Excuse </h1>
+      <button onClick={() => fetchData('party')}> Party </button>
+      <button onClick={() => fetchData('family')}> Family </button>
+      <button onClick={() => fetchData('office')}> Office </button>
+      <h2 className='excuse'> {excuse} </h2>
+    </div>
   )
 }
 
